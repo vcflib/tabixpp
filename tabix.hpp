7050628a97c7c4c871b179867029044b5344a147
Pjotr Prins <pjotr.public01@thebird.nl>
2023-01-12 00:02:20 -0600

    Get uncompressed file pos to be able to show a progress bar with vcflib

--- tabix.hpp
+++ tabix.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -24,6 +26,30 @@ class Tabix {
     bool has_jumped;
     vector<string>::iterator current_chrom;
 
+    /* uncompressed file pos
+    off_t hts_utell1(htsFile *fp)
+        {
+            if (fp->is_bgzf) {
+                return bgzf_htell(fp->fp.bgzf);
+            }
+            else
+                return htell(fp->fp.hfile);
+        }
+    */
+
+    // Get file position in compressed file - really on disk
+    off_t bgzf_htell1(BGZF *fp) {
+        if (fp->mt) {
+            return -1; // skip if multithreading
+            //pthread_mutex_lock(&fp->mt->job_pool_m);
+            //off_t pos = fp->block_address + fp->block_clength;
+            //pthread_mutex_unlock(&fp->mt->job_pool_m);
+            //return pos;
+        } else {
+            return htell(fp->fp);
+        }
+}
+
 public:
     string filename;
     vector<string> chroms;
@@ -37,5 +63,5 @@ public:
     bool setRegion(string& region);
     bool getNextLine(string& line);
     bool getNextLineKS();
-
+    long file_pos() { return bgzf_htell1(fn->fp.bgzf); };
 };
